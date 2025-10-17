74088c3fa9b41db003557e0ff2a861044e8e9884
RodrigoEMM11 <remm2003@gmail.com>
2025-10-17 00:49:42 -0300

    Agregue los codigos fuentes de la carpeta controller

---
+++ src/main/java/com/jrr/alquiler/controller/ClienteController.java
@@ -0,0 +1,25 @@
+package com.jrr.alquiler.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.jrr.alquiler.model.Cliente;
+import com.jrr.alquiler.repository.ClienteRepository;
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @PostMapping
+    public Cliente registrarCliente(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @GetMapping
+    public List<Cliente> listarClientes() {
+        return clienteRepository.findAll();
+    }
+}
