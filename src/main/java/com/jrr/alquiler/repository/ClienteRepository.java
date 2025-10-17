c7b998619a127f1a536472ab3431a2e0e36d2956
Jorge <jorge.adan.ferreira02@gmail.com>
2025-10-17 00:43:06 -0300

    Creacion de la base para el servidor

--- src/main/java/com/jrr/alquiler/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jrr.alquiler.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.jrr.alquiler.model.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
-
--- src/main/java/com/jrr/alquiler/repository/VehiculoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.jrr.alquiler.repository;
-
-import com.jrr.alquiler.model.Vehiculo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehiculoRepository extends JpaRepository<Vehiculo, Long> { }
